10402bcc432541a636ab7466b29af2fc8ce84aee
ramana0077 <venkatramanajandhyam@gmail.com>
2023-10-27 13:33:34 +0000

    new commits

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,9 +3,10 @@ echo "Running ApplicationStart script for Node.js application"
 
 # Navigate to the frontend directory and start the frontend with custom options
 cd /var/www/myapp/frontend
-npm run dev -- --host 0.0.0.0 &
+nohup npm run dev -- --host 0.0.0.0 > vite.log 2>&1 &
 
 # Navigate to the server directory and start the server
 cd /var/www/myapp/server
-npm start
+setsid npm start > start.log 2>&1 < /dev/null &
+
 
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -5,23 +5,10 @@ echo "Running ApplicationStop script for Node.js application"
 cd /var/www/myapp/frontend
 
 # Stop the frontend process (if running)
-pid_frontend=$(pgrep -f "npm run dev")
-if [ -n "$pid_frontend" ]; then
-    echo "Stopping the frontend process (PID: $pid_frontend)..."
-    kill -SIGTERM $pid_frontend
-else
-    echo "Frontend process is not running."
-fi
+# Kill all Node.js processes using 'killall'
+sudo killall -9 node
 
-# Navigate to the server directory
-cd /var/www/myapp/server
+# Note: Using 'killall -9' is a forceful way to terminate the processes.
+# Be cautious when using it, as it won't allow processes to clean up resources.
 
-# Stop the server process (if running)
-pid_server=$(pgrep -f "npm start")
-if [ -n "$pid_server" ]; then
-    echo "Stopping the server process (PID: $pid_server)..."
-    kill -SIGTERM $pid_server
-else
-    echo "Server process is not running."
-fi
 
