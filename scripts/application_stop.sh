2904fca4322ada0bfa8354e73d5737034e9795b7
jandhyam venkata ramana <venkatramanajandhyam@gmail.com>
2023-10-27 22:43:06 +0530

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -3,11 +3,9 @@ echo "Running ApplicationStop script for Node.js application"
 
 # Navigate to the frontend directory
 cd /var/www/myapp/frontend
-
-# Stop the frontend process (if running)
-# Kill all Node.js processes using 'killall'
 sudo killall -9 node
 
+echo "success"
 # Note: Using 'killall -9' is a forceful way to terminate the processes.
 # Be cautious when using it, as it won't allow processes to clean up resources.
 
