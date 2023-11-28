ba09616a58477f8d225fab1542cb60e805c947f7
ramana0077 <venkatramanajandhyam@instance-5.us-central1-a.c.vocal-circle-403808.internal>
2023-11-28 12:36:41 +0000

    updated frontend to port 80

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,6 +4,8 @@ echo "Running ApplicationStart script for Node.js application"
 # Navigate to the frontend directory and start the frontend with custom options
 cd /var/www/myapp/frontend
 nohup npm run dev -- --host 0.0.0.0 > vite.log 2>&1 &
+sudo ufw allow 5173
+
 
 # Navigate to the server directory and start the server
 cd /var/www/myapp/server
